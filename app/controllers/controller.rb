62244312b21440bdc34044bf059d040f868f6406
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 12:30:44 -0700

    create controller

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -5,22 +5,47 @@ class Controller
   class << self
 
     def run
-      input = get_user_input
-      self.send(input)
+      command, input = validate(get_user_input)
+      if task_num
+        self.send(command, input)
+      else
+        self.send(command)
+      end
     end
 
     def list
+      puts "Tasks:"
+      Task.all.with_index(1) do |task, index|
+        puts "#{index}. [#{task.status == :complete ? "X" : " "}] #{task.description}"
+      end
     end
 
-    def add
     end
 
-    def delete
+    def add(input)
     end
 
-    def 
+    def delete(input)
+    end
+
+    def complete(task_num)
+    end
 
     def get_user_input
+      ARGV
+    end
+
+    def validate(input)
+      puts "Invalid input" if input.length > 2
+      puts "Please use 'list', 'add', 'delete' or 'complete'" if input.length == 0
+      if input.length == 2
+        unless ['add', 'delete', 'complete'].include?(input[0])
+          puts "Invalid input"
+          exit
+        end
+        return input[0], input[1]
+      end
+      return input[0], nil if ['list'].include?(input[0])
     end
 
   end
