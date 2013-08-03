729afe9c62e4ccd11e525e1713b526ad20b5e65b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 17:34:15 -0700

    ADD_LISTgit add .

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,9 @@ class List < ActiveRecord::Base
   has_many :tasks
   class << self
 
-    def add_list
-
+    def add_list(name)
+      self.create({name: name, active_list: List.all.first.active_list})
+      puts "You've just created the following list: #{name}"
     end
 
     def all_lists(arg)
@@ -15,7 +16,6 @@ class List < ActiveRecord::Base
     end
 
     def select_list
-
       
     end
 
